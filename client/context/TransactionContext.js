fd9c5c06eaf8a96f0c9e08359ee69025b834a50d
outofbound4 <gaurav.kr109@gmail.com>
2022-07-14 01:29:08 +0530

    File created

---
+++ client/context/TransactionContext.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+export const TransactionContext = React.createContext()
+
+let eth
+
+if(typeof window != 'undefined') {
+    eth = window.ethereum
+}
+
+export const TrasactionProvider = ({children}) => {
+    const [currentAccount, setCurrentAccount] = useState()
+    const connectWallet = async (metamask = eth) => {
+        try {
+            if(!metamask)
+                return alert('Please install metamask')
+            const accounts = await metamask.request({ method: 'eth_requestAccounts' })
+            setCurrentAccount(accounts[0])
+        }
+        catch (error) {
+            console.error(error)
+            throw new Error('No ethereum Object')
+        }
+    }
+
+    return (
+        <TransactionContext.Provider
+            value = {{
+                currentAccount,
+                connectWallet,
+            }}
+        >
+            {children}
+        </TransactionContext.Provider>
+    )
+}
+
