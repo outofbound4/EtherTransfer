e536b3b11b2006b542cda30a820e92736827d1e7
outofbound4 <gaurav.kr109@gmail.com>
2022-07-14 01:29:36 +0530

    more component added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header'
+import Main from '../components/Main'
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen text-white select-none flex flex-col justify-between`,
 }
@@ -8,7 +9,7 @@ const Home: NextPage = () => {
   return (
     <div className={ style.wrapper }>
       <Header />
-      <h1>Bye</h1>
+      <Main />
     </div>
   )
 }
